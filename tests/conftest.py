95a1a9da8a4849d0ce1de0ea8672bafa01e0867e
Rui Pereira <ruiarpereira15@gmail.com>
2025-12-12 22:43:56 +0000

    :test_tube: Add actions tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,13 +1,32 @@
-import types
+import contextlib
 import pytest
+
 
 class FakeStreamlit:
     def __init__(self):
         self.session_state = {}
+
         self.warnings = []
+        self.errors = []
+        self.toasts = []
+        self.reruns = 0
 
     def warning(self, msg: str) -> None:
         self.warnings.append(msg)
+
+    def error(self, msg: str) -> None:
+        self.errors.append(msg)
+
+    def toast(self, msg: str, icon=None) -> None:
+        self.toasts.append((msg, icon))
+
+    def rerun(self) -> None:
+        self.reruns += 1
+
+    @contextlib.contextmanager
+    def spinner(self, msg: str):
+        yield
+
 
 @pytest.fixture()
 def fake_st():
